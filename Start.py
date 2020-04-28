c2fb75ed86e10647114be0106825e51944c9091a
johnhuzhy <johnho560@gmail.com>
2020-04-28 15:21:01 +0900

    0428 commit

--- Start.py
+++ Start.py
@@ -1,3 +1,6 @@
+import decimal
+import math
+
 print("Hello World!")
 
 print(id(33))
@@ -6,3 +9,10 @@ print(type(3.22222))
 print(9.8 * -7.2)
 print(2e3)
 print(5//2)
+
+a = decimal.Decimal(10.0)
+b = decimal.Decimal(3)
+print(a/b)
+#divmod(9/2)
+
+print(math.pi)
