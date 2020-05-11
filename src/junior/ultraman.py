edcfc6438371973905ee3e007c0205c2a116c685
johnhuzhy <johnho560@gmail.com>
2020-05-11 10:33:45 +0900

    05/11 ultraman修正

--- src/junior/ultraman.py
+++ src/junior/ultraman.py
@@ -36,10 +36,28 @@ class Fighter():
     def alive(self):
         return self._hp > 0
 
-    @abstractmethod
     def attack(self, other):
         """
         攻撃する
+        """
+        fb = randint(1, 10)
+        if fb >= 8:
+            self.fatal_blow(other)
+        else:
+            self.normal_attack(other)
+
+    @abstractmethod
+    def fatal_blow(self, other):
+        """
+        致命的な打撃
+        :param other: 攻撃されたもの
+        """
+        pass
+
+    @abstractmethod
+    def normal_attack(self, other):
+        """
+        一般的な攻撃
         :param other: 攻撃されたもの
         """
         pass
@@ -53,13 +71,18 @@ class Ultraman(Fighter):
         super().__init__(name, hp)
         self._mp = mp
 
-    def attack(self, other):
+    def normal_attack(self, other):
         other.hp -= randint(20, 25)
+
+    def fatal_blow(self, other):
+        injury = randint(40, 60)
+        other.hp -= injury
+        print(f'◆◆◆ {self._name}奥特曼は致命的な打撃を与える({injury}) ◆◆◆')
 
     def huge_attack(self, other):
         """
-        究極必杀技(打掉对方至少50点或四分之三的血)
-        :param other: 被攻击的对象
+        究極必殺技(打掉对方至少50点或四分之三的血)
+        :param other: 被攻撃的对象
         :return: 使用成功返回True否则返回False
         """
         if self._mp >= 50:
@@ -74,15 +97,15 @@ class Ultraman(Fighter):
 
     def magic_attack(self, others):
         """
-        魔法攻击
-        :param others: 被攻击的群体
+        魔法攻撃
+        :param others: 被攻撃的群体
         :return: 使用魔法成功返回True否则返回False
         """
         if self._mp >= 20:
             self._mp -= 20
             for temp in others:
                 if temp.alive:
-                    temp.hp -= randint(10, 15)
+                    temp.hp -= randint(15, 30)
             return True
         else:
             return False
@@ -101,15 +124,28 @@ class Monster(Fighter):
     """モンスター"""
     __slots__ = ('_name', '_hp')
 
-    def attack(self, other):
+    def normal_attack(self, other):
         other.hp -= randint(10, 20)
 
+    def fatal_blow(self, other):
+        if int(self._hp * 0.1) > 5:
+            blood = randint(5, int(self._hp * 0.1))
+            injury = randint(10, 20) + int(blood * 1.8)
+            self._hp -= blood
+            other.hp -= injury
+            print(f'◇◇◇　{self._name}小怪獸は血撃を与える({blood}↢↣{injury})　◇◇◇')
+        else:
+            self.normal_attack(other)
+
     def __str__(self):
-        return f'~~~{self._name}小怪兽~~~\n  生命值: {self._hp}\n'
+        if self.alive:
+            return f'~~~{self._name}小怪獸~~~\n  生命值: {self._hp}\n'
+        else:
+            return f'~~~{self._name}は殺された~~~\n'
 
 
 def is_any_alive(monsters):
-    """判断有没有小怪兽是活着的"""
+    """判断有没有小怪獸是活着的"""
     for temp in monsters:
         if temp.alive:
             return True
@@ -118,7 +154,7 @@ def is_any_alive(monsters):
 
 
 def select_one_alive(monsters):
-    """选中一只活着的小怪兽"""
+    """選擇一只活着的小怪獸"""
     mon_lens = len(monsters)
     while True:
         index = randrange(mon_lens)
@@ -134,38 +170,42 @@ def display_info(ultraman, monsters):
 
 
 if __name__ == "__main__":
-    u = Ultraman('Huzhy', 500, 100)
-    m1 = Monster('狄仁杰', 250)
-    m2 = Monster('白元芳', 300)
-    m3 = Monster('王大锤', 250)
+    u = Ultraman('Huzhy', 800, 100)
+    m1 = Monster('牛魔王', 250)
+    m2 = Monster('白骨精', 200)
+    m3 = Monster('犀牛精', 350)
     ms = [m1, m2, m3]
     fight_round = 1
     while u.alive and is_any_alive(ms):
         print('#'*15, f"第{fight_round:02}回合", '#'*15)
         m = select_one_alive(ms)
         skill = randint(1, 10)
-        if skill <= 6:  # 60%的概率使用普通攻击
-            print(f'{u.name}使用普通攻击打了{m.name}.')
+        if skill <= 6:  # 60%的概率使用普通攻撃
+            print(f'{u.name}使用普通攻撃打了{m.name}.')
             u.attack(m)
             print(f'{u.name}的魔法值回復了{u.resume()}点.')
-        elif skill <= 9:  # 30%的概率使用魔法攻击(可能因魔法值不足而失败)
+        elif skill <= 9:  # 30%的概率使用魔法攻撃(可能因魔法值不足而失败)
             if u.magic_attack(ms):
-                print(f'{u.name}使用了魔法攻击.')
+                print(f'{u.name}使用了魔法攻撃.')
             else:
                 print(f'{u.name}使用魔法失败.')
-        else:  # 10%的概率使用究極必杀技(如果魔法值不足则使用普通攻击)
+                print(f'{u.name}使用普通攻撃打了{m.name}.')
+                u.attack(m)
+        else:  # 10%的概率使用究極必殺技(如果魔法值不足则使用普通攻撃)
             if u.huge_attack(m):
-                print(f'{u.name}使用究極必杀技虐了{m.name}.')
+                print(f'{u.name}使用§究極必殺技§虐了{m.name}.')
             else:
-                print(f'{u.name}使用普通攻击打了{m.name}.')
+                print(f'{u.name}使用普通攻撃打了{m.name}.')
                 print(f'{u.name}的魔法值回復了{u.resume()}点.')
-        if m.alive:  # 如果选中的小怪兽没有死就回击奥特曼
-            print(f'{m.name}回击了{u.name}.')
-            m.attack(u)
-        display_info(u, ms)  # 每个回合结束后显示奥特曼和小怪兽的信息
+        if is_any_alive(ms):  # 如果小怪獸没有死就回击奥特曼
+            for mst in ms:
+                if mst.alive:
+                    print(f'{mst.name}回击了{u.name}.')
+                    mst.attack(u)
+        display_info(u, ms)  # 每个回合结束后显示奥特曼和小怪獸的信息
         fight_round += 1
     print("="*15, "战斗结束!", "="*15)
     if u.alive:
         print(f'{u.name}奥特曼勝利!')
     else:
-        print('小怪兽勝利!')
+        print('小怪獸勝利!')
