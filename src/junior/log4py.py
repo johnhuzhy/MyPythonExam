aa93505d375982e0087989bcc939a5a069f41949
johnhuzhy <johnho560@gmail.com>
2020-06-03 23:46:16 +0900

    結構調整

---
+++ src/junior/log4py.py
@@ -0,0 +1,7 @@
+from junior.util import log as lg
+
+lg.logger.debug('this is debug message')
+lg.logger.info('自作モジュールの置いてあるディレクトリパス')
+lg.logger.warning('from slackbot.bot import Bot')
+lg.logger.error('pip install slackbot')
+lg.logger.critical('cimportlib.import_module')
