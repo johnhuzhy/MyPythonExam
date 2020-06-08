5c243aee92c3349f0a8ff29dee1a4c1b9694dfc8
johnhuzhy <johnho560@gmail.com>
2020-06-08 14:58:00 +0900

    06/08 closure2

--- src/senior/exec_closure.py
+++ src/senior/exec_closure.py
@@ -10,18 +10,10 @@ def curve_pre():
         return y
     return curve
 
-a = 2
-fn = curve_pre()
-# タイプ
-print(type(fn))
-print(fn(7))
-# 環境変数
-print(fn.__closure__)
-print(fn.__closure__[0].cell_contents)
-print('#'*33)
 
 def f1():
     a = 12
+
     def f2():
         a = 13
         print(a)
@@ -29,4 +21,40 @@ def f1():
     f2()
     print(a)
 
-f1()
+
+def clsr_trvl(total=0):
+    def trvl_go(add):
+        nonlocal total
+        new_total = total + add
+        total = new_total
+        return new_total
+    return trvl_go
+
+
+def not_clsr_trvl(x):
+    global tol
+    new_tol = tol + x
+    tol = new_tol
+    return new_tol
+
+
+if __name__ == "__main__":
+    a = 2
+    fn = curve_pre()
+    # タイプ
+    print(type(fn))
+    print(fn(7))
+    # 環境変数
+    print(fn.__closure__)
+    print(fn.__closure__[0].cell_contents)
+    print('#'*33)
+
+    f1()
+    print('#'*33)
+
+    tol = 0
+    ft = clsr_trvl()
+    for idx in range(3):
+        a = int(input('ADD='))
+        print(not_clsr_trvl(a), "&& tol=", tol)
+        print(ft(a), "&& 環境変数=", ft.__closure__[0].cell_contents)
