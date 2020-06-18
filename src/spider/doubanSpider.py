f09143930f8b2d5851245ae7ab3fb3638187052c
johnhuzhy <johnho560@gmail.com>
2020-06-18 22:42:55 +0900

    06/18 commit

--- src/spider/doubanSpider.py
+++ src/spider/doubanSpider.py
@@ -6,7 +6,7 @@ class Spider():
     """♠主に教材と同じように練習する♠"""
     url = 'https://market.douban.com/book/?utm_campaign=book_nav_freyr&utm_source=douban&utm_medium=pc_web'
     item_path = '<li class="book-item">([\s\S]*?)</li>'
-    price_path ='<i>([\d|\.]*?)</i>'
+    price_path = '<i>([\d|\.]*?)</i>'
     name_path = '<h3>([\w\W]*?)</h3>'
 
     def __fetch_content(self):
@@ -17,14 +17,37 @@ class Spider():
 
     def __analysis(self, ht):
         item_html = re.findall(Spider.item_path, ht)
+        anhors = []
         for ih in item_html:
             name = re.findall(Spider.name_path, ih)
             price = re.findall(Spider.price_path, ih)
-            print(name, "⇒", price)
+            anhor = {'name': name[0], 'price': price[0]}
+            anhors.append(anhor)
+        return anhors
+
+    def __refine(self, anhors):
+        def lm(anhors): return {
+            'name': "《" + anhors['name'] + "》",
+            'price': float(anhors['price'])
+        }
+        return list(map(lm, anhors))
+
+    def __sort(self, anhors):
+        return sorted(anhors, key=self.__sort_item, reverse=True)
+
+    def __sort_item(self, anhors):
+        return anhors['price']
+
+    def __show(self, anhors):
+        for anhor in anhors:
+            print(anhor['name'], '⇒ ', anhor['price'], '元')
 
     def go(self):
         ht = self.__fetch_content()
-        self.__analysis(ht)
+        lst = self.__analysis(ht)
+        lst = self.__refine(lst)
+        lst = self.__sort(lst)
+        self.__show(lst)
 
 
 if __name__ == "__main__":
