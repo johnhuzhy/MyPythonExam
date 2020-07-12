334ff38f8943e1bd9c18de6e7b47dbc535ae631d
johnhuzhy <johnho560@gmail.com>
2020-07-12 23:26:18 +0900

    画面表示修正

--- src/MyWeb/routes/register_route.py
+++ src/MyWeb/routes/register_route.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Blueprint, redirect, url_for
+from flask import Flask, render_template, Blueprint, redirect, url_for, request
 
 app = Blueprint('register', __name__)
 
@@ -8,6 +8,16 @@ def register_input():
     return render_template('register_input.html')
 
 
-@app.route('/register_back', methods=['get', 'post'])
-def register_back():
-    return redirect(url_for('home'))
+@app.route('/register', methods=['get', 'post'])
+def register():
+    if request.method == 'POST':
+        print(request.form)
+        if request.form.get('Exec') == '会員登録':
+            return render_template("top.html")
+        elif request.form.get('Back') == '戻\u3000る':
+            return redirect(url_for('home'))
+        else:
+            print('入力エラー')
+            return redirect(url_for('home'))
+    else:
+        return render_template('register_input.html')
