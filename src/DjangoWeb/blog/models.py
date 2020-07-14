c87972cbf01f79ee6078c485f407162f840811aa
johnhuzhy <johnho560@gmail.com>
2020-07-14 09:55:02 +0900

    blog Init 修正

--- src/DjangoWeb/blog/models.py
+++ src/DjangoWeb/blog/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class BlogArticles(models.Model):
     title = models.CharField(max_length=64)
-    author = models.ForeignKey(User, related_name="blog_posts")
+    author = models.ForeignKey(User, related_name="blog_posts", on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        odering = ("-publish",)
+        ordering = ("-publish",)
 
     def __str__(self):
         return self.title
