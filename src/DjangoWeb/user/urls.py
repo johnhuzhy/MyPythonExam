477b1df07a4544fc40056496aa681012931eac12
johnhuzhy <johnho560@gmail.com>
2020-07-19 16:49:28 +0900

    2.4　サインアップ完了

--- src/DjangoWeb/user/urls.py
+++ src/DjangoWeb/user/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.user_login, name="user_login"),
     url(r'^logout/$', views.user_logout, name="user_logout"),
+    url(r'^register/$', views.user_register, name="user_register"),
+    url(r'^register_result/$', views.user_register_result, name="user_register_result"),
 ]
--- src/DjangoWeb/user/views.py
+++ src/DjangoWeb/user/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm
 
 
 def user_login(request):
+    """ログイン処理"""
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
@@ -25,6 +26,34 @@ def user_login(request):
 
 
 def user_logout(request):
+    """ログアウト処理"""
     if request.method == 'GET':
         auth.logout(request)
         return render(request, 'user/logout.html')
+
+
+def user_register(request):
+    """サインアップ処理"""
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponseRedirect('/user/register_result/')
+        else:
+            return HttpResponse("Sorry, you can not register!")
+    else:
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        return render(request, 'user/register.html', {'form': user_form, 'profile': userprofile_form})
+
+
+def user_register_result(request):
+    """サインアップ結果"""
+    if request.method == 'GET':
+        return render(request, 'user/register_result.html')
