4577629ad4914c3e2f2791a5113b43ede2022930
johnhuzhy <johnho560@gmail.com>
2020-07-22 13:30:12 +0900

    3.0 登録完了

--- src/DjangoWeb/article/form.py
+++ src/DjangoWeb/article/form.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import ArticleColumn
 
+
 class ArticleColumnForm(forms.ModelForm):
     class Meta:
         model = ArticleColumn
         fields = ('column',)
 
-class ArticleColumnAddForm(forms.ModelForm):
-    class Meta:
-        model = ArticleColumn
-        fields = ('column',)
+
+class ArticleColumnAddForm(forms.Form):
+    new_column = forms.CharField(label='新しいコラム', max_length=64)
--- src/DjangoWeb/article/views.py
+++ src/DjangoWeb/article/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from .models import ArticleColumn
-from .form import ArticleColumnForm
+from .form import ArticleColumnForm, ArticleColumnAddForm
 
 
 @login_required(login_url='/user/login/')
 @csrf_exempt
 def article_column(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
+        if 'add' in request.POST:
+            return HttpResponseRedirect('/article/article_column/add/')
+        elif 'upd' in request.POST:
+            return render(request, 'article/column_upd.html')
+        elif 'del' in request.POST:
+            return render(request, 'article/column_del.html')
+    else:
         columns = ArticleColumn.objects.filter(user=request.user)
         column_form = ArticleColumnForm
-        return render(request, 'article/column.html', {'columns': columns, 'column_form':column_form})
-    elif request.method == 'POST':
-        return render(request, 'article/column_add.html')
+        return render(request, 'article/column.html', {'columns': columns, 'column_form': column_form})
 
 
 @login_required(login_url='/user/login/')
 @csrf_exempt
 def article_column_add(request):
-    if request.method == 'GET':
-        columns = ArticleColumn.objects.filter(user=request.user)
-        column_form = ArticleColumnForm
-        return render(request, 'article/column_add.html', {'columns': columns, 'column_form':column_form})
-    elif request.method == 'POST':
-        return render(request, 'article/column.html', {'columns': columns, 'column_form':column_form})
+    if request.method == 'POST':
+        new_column_name = request.POST['new_column']
+        columns = ArticleColumn.objects.filter(user=request.user, column=new_column_name)
+        if columns:
+            column_add_form = ArticleColumnAddForm(request.POST)
+            column_add_form.add_error('new_column','コラムは既に存在しているので、ご確認ください。')
+            return render(request, 'article/column_add.html', {'form': column_add_form, })
+        else:
+            ArticleColumn.objects.create(user=request.user, column=new_column_name)
+            return HttpResponseRedirect('/article/article_column/')
+    else:
+        column_add_form = ArticleColumnAddForm
+        return render(request, 'article/column_add.html', {'form': column_add_form})
